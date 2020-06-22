74cb9f72ceb339611d3588e1aae32f8b09ea72d1
hiiri <aleksicohen@gmail.com>
2020-06-22 15:06:34 +0300

    Change ex00 text to scale based on height, fix warning in ex02

--- d01/ex02/oddeven.php
+++ d01/ex02/oddeven.php
@@ -4,7 +4,7 @@
 while (true)
 {
 	echo "Enter a number: ";
-	$stdin = fopen('php://stdin', r);
+	$stdin = fopen('php://stdin', "r");
 	$input = fgets($stdin);
 	if ($input == NULL)
 	{
@@ -13,12 +13,11 @@ while (true)
 	}
 	$input = trim($input);
 
-
 	if (!is_numeric($input))
 	echo "'".$input."' is not a number\n";
 	else if ($input % 2 == 0)
 	echo "The number ".$input." is even\n";
-	else if ($input % 2 == 1)
+	else
 	echo "The number ".$input." is odd\n";
 	fclose ($stdin);
 }
