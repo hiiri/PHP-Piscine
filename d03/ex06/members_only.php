357014b5aacd38bc28da5cdb03a1ea625ff97696
Aleksi Cohen <alcohen@student.hive.fi>
2020-06-26 17:52:01 +0300

    Change name to uppercase

--- d03/ex06/members_only.php
+++ d03/ex06/members_only.php
@@ -6,15 +6,14 @@
 	{
 		header('Content-Type: text/html');
 		$image = file_get_contents('../img/42.png');
-		echo "<html><body>\nHello $user<br />\n";
+		echo "<html><body>\nHello Zaz<br />\n";
 		echo "<img src='data:image/png;base64,".base64_encode($image)."'>\n";
 		echo "</body></html>\n";
 	}
 	else
 	{
 		header("WWW-Authenticate: Basic realm=''Member area''");
-		header("HTTP/1.1 401 Unauthorized");
-		header('Content-Type: text/html');
+		header("HTTP/1.0 401 Unauthorized");
 		echo "<html><body>That area is accessible for members only</body></html>\n";
 	}
 	header("Connection: close");
